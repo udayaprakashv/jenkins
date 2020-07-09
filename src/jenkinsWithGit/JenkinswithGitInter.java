8515a65cdd9f42a69f2b30949b28cd82ddac1fcf
Udayaprakash Venkatapathy <U44052@ust-global.com>
2020-07-09 16:18:07 +0530

    for jenkins added new methods

--- src/jenkinsWithGit/JenkinswithGitInter.java
+++ src/jenkinsWithGit/JenkinswithGitInter.java
@@ -5,5 +5,9 @@ public class JenkinswithGitInter {
 	public static void main(String[] args) {
 		System.out.println("Sample project to Jenkins connected");
 	}
+	
+	public void callMehtod() {
+		System.out.println("call method added for jakins");
+	}
 
 }
